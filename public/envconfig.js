5f430bfb27b311758e19ec18e743d25352d818d8
Abel Copa <abelw.copa@gmail.com>
2022-02-17 18:03:55 -0400

    I CAMBIO DE IP

--- public/envconfig.js
+++ public/envconfig.js
@@ -1,5 +1,5 @@
 window.globalConfig = {
-    endpoint_riesgos: 'http://192.168.1.6:8081/', //10.17.21.238
+    endpoint_riesgos: 'https://127.0.0.1:5452/riesgos-0.0.1/', //10.17.21.238
     sis: 'sistema_riesgos',
     sessionTimer: 15 // Tiempo en minutos
 };
