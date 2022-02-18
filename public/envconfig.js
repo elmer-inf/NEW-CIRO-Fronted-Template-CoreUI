b06205b920d4942c5de12f05cfef0ccd6bd25a5c
Abel Copa <abelw.copa@gmail.com>
2022-02-18 10:21:32 -0400

    Se corige el nonbre del sistema, ya que en producicon dio error

--- public/envconfig.js
+++ public/envconfig.js
@@ -1,5 +1,5 @@
 window.globalConfig = {
     endpoint_riesgos: 'https://172.16.180.130:5462/riesgos-0.0.1/', //10.17.21.238
-    sis: 'sistema_riesgos',
+    sis: 'sistema_de_riesgos',
     sessionTimer: 15 // Tiempo en minutos
 };
