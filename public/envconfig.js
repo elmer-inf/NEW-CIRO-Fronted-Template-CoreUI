d5fbca22701bdff4f60c805a82330cd5d6ae405b
Elmer Machaca <is.elmer.inf@gmail.com>
2026-02-26 17:08:48 -0400

    116363 - bug en estados de eventos y salto de linea en ciro

--- public/envconfig.js
+++ public/envconfig.js
@@ -3,4 +3,4 @@ window.globalConfig = {
     sis: 'sistema_de_riesgos',
     sessionTimer: 15 // Tiempo en minutos
 };
-//'https://172.16.180.130:5452/riesgos-0.0.1/', //10.17.21.238
+//'https://172.16.180.130:5452/riesgos-0.0.1/', //10.17.21.238 
